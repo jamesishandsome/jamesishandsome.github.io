a121fd00b46d73de96684cca44081ba9e1e6f517
jameshu <james.hu@barecovetech.com>
2024-10-03 15:11:41 +0800

    init: reinit project

--- src/pages/Wasm/wasm/target/release/build/wasm-bindgen-5041a953651a0574/build_script_build-5041a953651a0574.d
+++
@@ -1,5 +0,0 @@
-/Users/jameshu/JamesProjects/jamesishandsome.github.io/src/pages/Wasm/wasm/target/release/build/wasm-bindgen-5041a953651a0574/build_script_build-5041a953651a0574: /Users/jameshu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/wasm-bindgen-0.2.88/build.rs
-
-/Users/jameshu/JamesProjects/jamesishandsome.github.io/src/pages/Wasm/wasm/target/release/build/wasm-bindgen-5041a953651a0574/build_script_build-5041a953651a0574.d: /Users/jameshu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/wasm-bindgen-0.2.88/build.rs
-
-/Users/jameshu/.cargo/registry/src/index.crates.io-6f17d22bba15001f/wasm-bindgen-0.2.88/build.rs:
