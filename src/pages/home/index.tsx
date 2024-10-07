765c9cdb806b77b546199a869c3182777a27ef5d
James Hu <me@jameshu.me>
2024-10-08 03:07:42 +0800

    feat: add 3d

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,42 +1,11 @@
-import { useRef, useState } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
-import { Mesh } from 'three'
-
-interface BoxProps {
-    position: [number, number, number]
-}
-
-function Box(props: BoxProps) {
-    // This reference gives us direct access to the THREE.Mesh object
-    const ref = useRef<Mesh>(null)
-    // Hold state for hovered and clicked events
-    const [hovered, hover] = useState(false)
-    const [clicked, click] = useState(false)
-    // Subscribe this component to the render-loop, rotate the mesh every frame
-    useFrame((_, delta) => {
-        if (ref.current) {
-            ref.current.rotation.x += delta
-        }
-    })
-    // Return the view, these are regular Threejs elements expressed in JSX
-    return (
-        <mesh
-            {...props}
-            ref={ref}
-            scale={clicked ? 1.5 : 1}
-            onClick={() => click(!clicked)}
-            onPointerOver={() => hover(true)}
-            onPointerOut={() => hover(false)}
-        >
-            <boxGeometry args={[1, 1, 1]} />
-            <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-        </mesh>
-    )
-}
+import { Canvas } from '@react-three/fiber'
+import { useLoader } from '@react-three/fiber'
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js'
 
 const Home = () => {
+    const obj = useLoader(OBJLoader, '/models/obj/Lowpoly_Notebook_2.obj')
     return (
-        <Canvas>
+        <Canvas camera={{ fov: 75, near: 0.1, far: 1000, position: [0, 5, 5] }}>
             <ambientLight intensity={Math.PI / 2} />
             <spotLight
                 position={[10, 10, 10]}
@@ -50,8 +19,7 @@ const Home = () => {
                 decay={0}
                 intensity={Math.PI}
             />
-            <Box position={[-1.2, 0, 0]} />
-            <Box position={[1.2, 0, 0]} />
+            <primitive object={obj} />
         </Canvas>
     )
 }
