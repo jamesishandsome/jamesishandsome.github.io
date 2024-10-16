6ad54eab8ba276e5b4c6911566c50c69b255084f
jameshu <james.hu@barecovetech.com>
2024-10-16 17:46:45 +0800

    feat: environment

--- src/application/index.tsx
+++ src/application/index.tsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useRef } from 'react'
-import { Canvas, useFrame, useThree, useLoader } from '@react-three/fiber'
-import { Box, CameraControls, OrbitControls } from '@react-three/drei'
+import { Canvas, useFrame, useThree } from '@react-three/fiber'
 import { Vector3 } from 'three'
-import { useControllableValue, useMouse } from 'ahooks'
-import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js'
+import { useControllableValue } from 'ahooks'
+import { useCubeTexture, useGLTF, Environment } from '@react-three/drei'
+
 const Scene = () => {
     const [focus, setFocus] = React.useState(false)
     useEffect(() => {
@@ -13,8 +13,21 @@ const Scene = () => {
         <>
             <Rig focus={focus} />
             <ambientLight />
+            <CubeTexture />
             <JamesMesh focus={focus} onChange={setFocus} />
         </>
+    )
+}
+
+const CubeTexture = () => {
+    return (
+        <Environment
+            // background={false}
+            // background
+            // files={['px.jpg', 'nx.jpg', 'py.jpg', 'ny.jpg', 'pz.jpg', 'nz.jpg']}
+            // path={'/textures/background/'}
+            preset="city"
+        />
     )
 }
 
@@ -26,42 +39,254 @@ const JamesMesh = (props: {
         defaultValue: false,
     })
     const { pointer } = useThree()
-    const obj = useLoader(
-        OBJLoader,
-        '/models/obj/Lowpoly_Notebook_2.obj',
-        () => {},
-        (xhr) => {
-            console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
-        }
-    )
+
+    const { nodes, materials } = useGLTF('/models/office/scene.gltf')
 
     const boxRef = useRef()
     return (
-        <primitive
-            object={obj}
-            onClick={(e) => {
-                console.log('click')
-                if (!focus) setFocus(true)
-            }}
-            onPointerEnter={(e) => {
-                console.log('enter')
-                // change cursor to pointer
-                document.body.style.cursor = 'pointer'
-            }}
-            onPointerLeave={(e) => {
-                console.log('leave')
-                document.body.style.cursor = 'auto'
-            }}
-            onPointerMissed={() => {
-                console.log('missed')
-                if (focus) setFocus(false)
-            }}
-            ref={boxRef}
-            args={[1, 1, 1]}
-            rotation={[0, Math.PI / 2, 0]}
-        >
-            <meshNormalMaterial />
-        </primitive>
+        <group {...props} dispose={null}>
+            <group rotation={[-Math.PI / 2, 0, Math.PI / 2]}>
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.Object_2.geometry}
+                    material={materials.Big_Wallpaper}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.Object_3.geometry}
+                    material={materials.Cactus}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.Object_4.geometry}
+                    material={materials.Drawer}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.Object_5.geometry}
+                    material={materials.File_Stand}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.Object_6.geometry}
+                    material={materials.File_Storage}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.Object_7.geometry}
+                    material={materials.Keyboard}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.Object_8.geometry}
+                    material={materials.Lamp}
+                />
+                <mesh
+                    onClick={() => {
+                        console.log('computer')
+                    }}
+                    // onClick={(e) => {
+                    //     if (!focus) setFocus(true)
+                    // }}
+                    onPointerEnter={() => {
+                        // change cursor to pointer
+                        document.body.style.cursor = 'pointer'
+                    }}
+                    onPointerLeave={() => {
+                        document.body.style.cursor = 'auto'
+                    }}
+                    // onPointerMissed={() => {
+                    //     if (focus) setFocus(false)
+                    // }}
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.Object_9.geometry}
+                    material={materials.Monitor}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.Object_10.geometry}
+                    material={materials.material_15}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.Object_11.geometry}
+                    material={materials.material_16}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.Object_12.geometry}
+                    material={materials.Pencil}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.Object_13.geometry}
+                    material={materials.Photo}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.Object_14.geometry}
+                    material={materials.PinBoard}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.Object_15.geometry}
+                    material={materials.Shelf01}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.Object_16.geometry}
+                    material={materials.Shelf02}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.Object_17.geometry}
+                    material={materials.Shelf03}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.Object_18.geometry}
+                    material={materials.Stool}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.Object_19.geometry}
+                    material={materials.Table}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.Object_20.geometry}
+                    material={materials.Tape}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.Object_21.geometry}
+                    material={materials.Telephone}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.Object_22.geometry}
+                    material={materials.Walls}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.Object_23.geometry}
+                    material={materials.Water_Cooler}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.Object_24.geometry}
+                    material={materials.Big_Bottle}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.Object_25.geometry}
+                    material={materials.Big_Bottle}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.Object_26.geometry}
+                    material={materials.material}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.Object_27.geometry}
+                    material={materials.File01}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.Object_28.geometry}
+                    material={materials.File02}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.Object_29.geometry}
+                    material={materials.File03}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.Object_30.geometry}
+                    material={materials.File04}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.Object_31.geometry}
+                    material={materials.File_Stack}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.Object_32.geometry}
+                    material={materials.Paper01}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.Object_33.geometry}
+                    material={materials.Paper02}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.Object_34.geometry}
+                    material={materials.Paper03}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.Object_35.geometry}
+                    material={materials.Paper_Cup}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.Object_36.geometry}
+                    material={materials.Paper_Stack}
+                />
+                <mesh
+                    castShadow
+                    receiveShadow
+                    geometry={nodes.Object_37.geometry}
+                    material={materials.material_24}
+                />
+            </group>
+        </group>
+        // <primitive
+        //     object={office.scene}
+        //
+        //     ref={boxRef}
+        //     args={[1, 1, 1]}
+        //     rotation={[0, Math.PI / 2, 0]}
+        // ></primitive>
     )
 }
 
@@ -77,7 +302,10 @@ function Rig(props: { focus: boolean }) {
         })
     } else {
         return useFrame(() => {
-            camera.position.lerp(vec.set(pointer.x * 3, pointer.y * 3, 6), 0.1)
+            camera.position.lerp(
+                vec.set(-10 + pointer.x * 3, 10 + pointer.y * 3, -10),
+                0.1
+            )
             camera.lookAt(0, 0, 0)
         })
     }
@@ -85,7 +313,11 @@ function Rig(props: { focus: boolean }) {
 
 const App = () => {
     return (
-        <Canvas camera={{ position: [0, 0, 6] }}>
+        <Canvas
+            camera={{
+                position: [-10, 10, -10],
+            }}
+        >
             <Scene />
         </Canvas>
     )
