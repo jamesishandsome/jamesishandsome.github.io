71d39237c8ddaf76515a806047202d3fe680502b
jameshu <james.hu@barecovetech.com>
2025-03-04 16:49:03 +0800

    feat: integrate react-router and update App component styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-const App = ( )=>{
-    return (
-        <div>
-        <h1>App</h1>
-        </div>
-    )
-}
+const App = () => {
+  return (
+    <div>
+      <h1 className={"w-full text-green-400"}>App</h1>
+    </div>
+  );
+};
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 import * as path from "node:path";
-import tailwindcss from '@tailwindcss/vite'
-
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
